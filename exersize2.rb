9c309cca5835ec414fe6a4a3c8e5391d2ba17bc5
Adam Tse <mr.adam.tse@gmail.com>
2016-08-09 13:36:44 -0400

    finished exces 2

--- exersize2.rb
+++ exersize2.rb
@@ -1,3 +1,9 @@
 tip = 55 * 0.15
 final = tip.floor
 puts "A good tip for 55 dollar meal is #{final}"
+puts "$" + final.to_s + " is a great one"
+
+puts ""
+puts ""
+
+puts "this 45628 + 7839 = #{7839+45628}"
