2bedc71a228af2c7caae90fc08903c729c1cf09c
Adam Tse <mr.adam.tse@gmail.com>
2016-08-09 13:58:03 -0400

    added name and age problem to exc 3

---
+++ excersize3.rb
@@ -0,0 +1,6 @@
+puts "What is your name?"
+name = gets.chomp
+puts "hello #{name}, nice to meet you!"
+puts "now how old are you?"
+age = gets.chomp
+puts "very nice were born in #{2016 - age.to_i}"
